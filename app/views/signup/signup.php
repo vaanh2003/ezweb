0f23d0053fddd6da7023543bcb9c4bbea8dcc3aa
Phong Phan Vo Trong <phongpvtps25229@fpt.edu.vn>
2023-06-10 05:00:29 +0700

    Fix login, signup

--- app/views/signup/signup.php
+++ app/views/signup/signup.php
@@ -45,34 +45,33 @@ form div {
     <div class="container">
         <div class="col-md-5">
             <?php
-                if (isset($_SESSION['error'])) {
-                    $message =  $_SESSION['error'];
-                    echo '<span style="color:red;">' . $message . '</span>';
-                }
-                unset($_SESSION['error']);
-                ?>
+            if (isset($_SESSION['error'])) {
+                $message =  $_SESSION['error'];
+                echo '<span style="color:red;">' . $message . '</span>';
+            }
+            unset($_SESSION['error']);
+            ?>
             <form role="form" method="post" action="<?php echo base_url; ?>addUser">
                 <fieldset>
                     <h1 class="text-uppercase pull-center">ĐĂNG KÝ</h1>
                     <div class="form-group">
                         <input type="text" name="username" id="username" class="form-control input-lg"
-                            placeholder="username">
+                            placeholder="Họ và tên">
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="email" id="email" class="form-control input-lg"
-                            placeholder="Email Address">
+                        <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email">
                     </div>
                     <div class="form-group">
                         <input type="password" name="password" id="password" class="form-control input-lg"
-                            placeholder="Password">
+                            placeholder="Mật khẩu">
                     </div>
                     <div class="form-group">
                         <input type="password" name="password2" id="password2" class="form-control input-lg"
-                            placeholder="Password2">
+                            placeholder="Nhập lại mật khẩu">
                     </div>
                     <div>
-                        <input type="submit" class="btn btn-lg btn-primary   value=" Register>
+                        <input type="submit" class="btn btn-lg btn-primary" value="Đăng ký">
                     </div>
                 </fieldset>
             </form>
