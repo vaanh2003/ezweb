9b94af39b6a4ccd3edae3f475b2f68942175ea5a
Phong Phan Vo Trong <phongpvtps25229@fpt.edu.vn>
2023-06-10 04:40:27 +0700

    login, signup

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace larava\controllers;
+
+use larava\core\Controller;
+
+class LoginController extends Controller
+{
+    public $user;
+    public function __construct()
+    {
+        $this->user = $this->Model('UserModel');
+    }
+    public function login()
+    {
+        $this->View("login/login");
+    }
+    public function getlogin()
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $check = $this->user::where('email', $email)->where('password', $password)->first();
+        if ($check) {
+            $userinfo = $check->getAttributes();
+            $_SESSION['username'] = $userinfo;
+            header("location:home");
+        } else {
+            $_SESSION['message'] = 'Tên đăng nhập hoặc mật khẩu không đúng';
+            header('location:login');
+        }
+    }
+    public function logout()
+    {
+        unset($_SESSION['username']);
+        header('location:home');
+    }
+}
